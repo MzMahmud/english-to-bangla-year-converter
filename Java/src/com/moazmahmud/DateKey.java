acbca4b7849dfa826cb10b8d9b79dd251585d1f1
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-30 09:23:53 +0600

    added DateKey class

---
+++ Java/src/com/moazmahmud/DateKey.java
@@ -0,0 +1,46 @@
+package com.moazmahmud;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class DateKey implements Comparable<DateKey> {
+    public int day;
+    public int month;
+    public int year;
+
+    public DateKey(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DateKey dateKey = (DateKey) o;
+        return day == dateKey.day && month == dateKey.month && year == dateKey.year;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(day, month, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Date{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
+
+    @Override
+    public int compareTo(DateKey other) {
+        return Comparator.comparing((DateKey d) -> d.year)
+                         .thenComparing(d -> d.month)
+                         .thenComparing(d -> d.day)
+                         .compare(this, other);
+    }
+}
